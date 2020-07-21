8a4a1a18dd86ac69c096b4509dd132ccfb2739fd
Mateus Mendonca <mateus.mendonca@softruck.com>
2020-07-21 20:04:44 -0300

    refactor: move http response to a helper method

--- src/presentation/controllers/search/search.ts
+++ src/presentation/controllers/search/search.ts
@@ -1,15 +1,13 @@
 import { MissingParamError } from '../../errors'
 import { HttpRequest, HttpResponse } from '../../protocols'
+import { badRequest } from '../../helpers/http/http-helper'
 
 export class SearchController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['search', 'limit']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError(field)
-        }
+        return badRequest(new MissingParamError(field))
       }
     }
   }
